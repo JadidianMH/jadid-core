f763a4c82c0165bb0b513cf232cd544e2b9e8a02
JadidianMH <mhmtsze@gmail.com>
2025-08-18 20:54:13 +0330

    some updates that i forgot what id did

--- includes/debug.h
+++ includes/debug.h
@@ -6,7 +6,9 @@
 struct LogEntry {
     std::string text;
     SDL_Texture* texture;
+    int width;
     int height;
 };
 void DrawDebug(SDL_Renderer* renderer, int wh, TTF_Font* font);
 void AddLog(const std::string& message);
+void ClearLogs();
--- src/engine/render/debug.cpp
+++ src/engine/render/debug.cpp
@@ -8,7 +8,7 @@ using namespace std;
 
 
 std::deque<LogEntry> logs;
-const size_t maxLogs = 29;
+const size_t maxLogs = 6;
 
 void DrawDebug(SDL_Renderer* renderer, int wh, TTF_Font* font)
 {
@@ -22,17 +22,31 @@ void DrawDebug(SDL_Renderer* renderer, int wh, TTF_Font* font)
             SDL_Surface* surf = TTF_RenderText_Blended(font, log.text.c_str(), color);
             log.texture = SDL_CreateTextureFromSurface(renderer, surf);
             log.height = surf->h;
+            log.width = surf->w;
             SDL_FreeSurface(surf);
         }
 
-        SDL_Rect dst = {10, y, 0, 0};
-        SDL_QueryTexture(log.texture, nullptr, nullptr, &dst.w, &dst.h);
+        SDL_Rect dst = {10, y, log.width, log.height};
         SDL_RenderCopy(renderer, log.texture, nullptr, &dst);
 
         y += log.height + 2;
     }
 }
-void AddLog(const std::string& message) {
+void AddLog(const std::string& message)
+{
     logs.push_back({message});
-    if (logs.size() > maxLogs) logs.pop_front();
+    if (logs.size() > maxLogs)
+    {
+        if (logs.front().texture)
+        {
+            SDL_DestroyTexture(logs.front().texture);
+        }
+        logs.pop_front();
+    }
+}
+void ClearLogs() {
+    for (auto& log : logs) {
+        if (log.texture) SDL_DestroyTexture(log.texture);
+    }
+    logs.clear();
 }
