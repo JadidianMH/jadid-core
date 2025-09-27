ed90d482899f95fdf3da84413420af6cc68057b2
JadidianMH <mhmtsze@gmail.com>
2025-09-27 18:49:02 +0330

    the code is more tidy and clean

--- includes/game_object.h
+++ includes/game_object.h
@@ -1,48 +1,54 @@
 #pragma once
 #include <string>
-#include <utility>
 #include <vector>
+#include <algorithm>
 #include "component.h"
+#include "sprite_renderer.h"
 #include "debug.h"
-#include "renderQueue.h"
+#include <SDL2/SDL.h>
 
 class GameObject
 {
 private:
     std::vector<Component*> components;
     std::string nameGameObject;
+    static std::vector<GameObject*> allGameObjects;
+
 public:
-    GameObject(const std::string& name)
-    {
-        nameGameObject = name;
-        RegisterGameObject(this);
-    }
-    ~GameObject()
-    {
-        for (auto component : components) delete component;
-    }
+    GameObject(const std::string& name);
+    ~GameObject();
 
     template <typename T, typename... Args>
     T* AddComponent(Args&&... args) {
         T* comp = new T(this, std::forward<Args>(args)...);
         components.push_back(comp);
-        AddLog("Added component to "+ nameGameObject +".");
+        AddLog("Added component to " + nameGameObject + ".");
         return comp;
     }
 
-
-    template<typename T>
-    T* GetComponent()
-    {
+    template <typename T>
+    T* GetComponent() {
         for (auto comp : components)
-        {
             if (T* casted = dynamic_cast<T*>(comp))
-            {
                 return casted;
-            }
-        }
-
         return nullptr;
     }
 
+    static void RenderAllSprites(SDL_Renderer* renderer) {
+        std::vector<GameObject*> renderableObjects;
+        for (auto go : allGameObjects)
+            if (go->GetComponent<SpriteRenderer>())
+                renderableObjects.push_back(go);
+
+        std::sort(renderableObjects.begin(), renderableObjects.end(),
+                  [](GameObject* a, GameObject* b) {
+                      return a->GetComponent<SpriteRenderer>()->zIndex <
+                             b->GetComponent<SpriteRenderer>()->zIndex;
+                  });
+
+        for (auto go : renderableObjects)
+            go->GetComponent<SpriteRenderer>()->Render(renderer);
+    }
+
+    static const std::vector<GameObject*>& GetAllGameObjects() { return allGameObjects; }
 };
--- src/engine/object/game_object.cpp
+++ src/engine/object/game_object.cpp
@@ -1,1 +1,15 @@
 #include "game_object.h"
+
+std::vector<GameObject*> GameObject::allGameObjects;
+
+GameObject::GameObject(const std::string& name)
+    : nameGameObject(name)
+{
+    allGameObjects.push_back(this);
+}
+
+GameObject::~GameObject()
+{
+    for (auto comp : components)
+        delete comp;
+}
