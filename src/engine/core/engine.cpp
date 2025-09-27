5ee8dd3734bc810845560f1d92e232171450163e
JadidianMH <mhmtsze@gmail.com>
2025-09-27 18:47:40 +0330

    renamed and refactored event_manager to input_manager in engine.cpp

--- src/engine/core/engine.cpp
+++ src/engine/core/engine.cpp
@@ -2,7 +2,7 @@
 #include "prepare_sdl.h"
 #include "config.h"
 #include <iostream>
-#include "event_manager.h"
+#include "input_manager.h"
 #include "debug.h"
 #include "game_object.h"
 #include "loader.h"
@@ -29,64 +29,21 @@ namespace Engine {
     }
 
     // Run the engine
-    void RunEngine(SDL_Window* window, SDL_Renderer* renderer, bool DEBUG = false) {
-
-        AddLog("Engine started");
-
+    void RunEngine(SDL_Window* window, SDL_Renderer* renderer, const bool DEBUG = false) {
         TTF_Font* font = TTF_OpenFont("font.ttf", 16);
-
-        int width, height;
         bool running = true;
-        SDL_Event event;
-        int frames = 0;
 
-        int w,h;
+        while (running) {
+            Input::Update();
+            if (Input::QuitRequested()) running = false;
 
-        auto* player = new GameObject("player");
-
-        auto* playerTransform = player->AddComponent<Transform>();
-        player->AddComponent<SpriteRenderer>("player.png", 5);
-        playerTransform->scale({.2,.2});
-
-
-        playerTransform->setPosition({0,0});
-
-        auto* ground = new GameObject("ground");
-        auto* groundTransform = ground->AddComponent<Transform>();
-        auto* groundSprite = ground->AddComponent<SpriteRenderer>("bg.png", 2);
-
-        groundTransform->setPosition({0,0});
-
-
-        while (running)
-        {
-            running = LoopManager::EventControl(&event, playerTransform);
-
-            SDL_GetWindowSize(window, &width, &height);
-
-            SDL_SetRenderDrawColor(renderer, 5, 60, 110, 255);
+            SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
             SDL_RenderClear(renderer);
-
-
-            frames++;
-
-            SDL_GetWindowSize(window, &w, &h);
-
-            playerTransform->scale({.1f,.1f});
-
-
-            RenderAllSprites(renderer);
-
-            if (DEBUG) DrawDebug(renderer, height, font);
-
 
             SDL_RenderPresent(renderer);
         }
 
         TTF_CloseFont(font);
-
-        AddLog("Quiting the engine");
-        ClearLogs();
     }
 
     // Quit the engine
